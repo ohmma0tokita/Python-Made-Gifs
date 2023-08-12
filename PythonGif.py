b7db6bd680d0ee34cbc2f2b555f3076b28ef977b
R.I.P <87244980+ohmma0tokita@users.noreply.github.com>
2023-08-12 13:15:00 +0100

    Update and rename dancing_anime.py to PythonGif.py

---
+++ PythonGif.py
@@ -0,0 +1,2 @@
+from dancing_gif import main
+main("frames-folder-name", music_file="music-file",speed="transition-speed")
--- dancing_anime.py
+++
@@ -1,2 +0,0 @@
-from dancing_gif import main
-main("anime", music_file="music_2.wav",speed=0.07)
